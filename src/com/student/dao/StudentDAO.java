b30c07791e6c7b49e3221082b2d9d2af6932f204
DesaleRohit <rohitdesale2005@gmail.com>
2025-11-21 10:04:50 +0530

    [MODIFIED]:Rohit - Add INSERT query to JDBC and implement conditional logic in MainApp

--- src/com/student/dao/StudentDAO.java
+++ src/com/student/dao/StudentDAO.java
@@ -1,5 +1,31 @@
 package com.student.dao;
 
-public class StudentDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+import com.student.db.DatabaseConnection;
+import com.student.model.Student;
+
+public class StudentDAO {
+	//ADD Student
+	public void addStudent(Student s) {
+		try {
+			Connection connection = DatabaseConnection.getConnection();
+			String query = "INSERT INTO students(name, age, email) VALUES(?,?,?)";
+			PreparedStatement ps = connection.prepareStatement(query);
+			
+			ps.setString(1, s.getName());
+			ps.setInt(2, s.getAge());
+			ps.setString(3, s.getEmail());
+			
+			ps.executeUpdate();
+			System.out.println("Student Added!");
+			
+			ps.close();
+			connection.close();
+			
+		} catch (Exception e) {
+			System.out.println("Error Adding Student!!!");
+		}
+	}
 }
--- src/com/student/main/MainApp.java
+++ src/com/student/main/MainApp.java
@@ -1,10 +1,43 @@
 package com.student.main;
 
+import java.util.Scanner;
+import com.student.dao.StudentDAO;
+import com.student.model.Student;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to CRUD Operation project");
+		
+		Scanner scanner = new Scanner(System.in);
+		StudentDAO dao = new StudentDAO();
+		
+		while (true) {
+			
+			System.out.println("\n1. Add");
+            System.out.println("2. View All");
+            System.out.println("3. Search");
+            System.out.println("4. Update");
+            System.out.println("5. Delete");
+            System.out.println("6. Exit");
+            System.out.print("Choice: ");
+            
+            int ch = scanner.nextInt();
+            
+            if (ch == 1) {
+            	scanner.nextLine();
+            	System.out.println("Name : ");
+            	String name = scanner.nextLine();
+            	System.out.println("Age : ");
+            	int age = scanner.nextInt();
+            	scanner.nextLine();
+            	System.out.println("Email : ");
+            	String email = scanner.nextLine();
+            	
+            	dao.addStudent(new Student(name, age, email));
+            }
+		}
 	}
 
 }
